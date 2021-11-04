ebab6e0de20320742d808a0c3398e54805b8ffab
Fikran Akbar <fikranakbar756@gmail.com>
2021-11-05 00:10:58 +0700

    fix text field behaviour in profile and change password fragment

--- app/src/main/java/com/glints/lingoparents/ui/accountsetting/profile/ProfileFragment.kt
+++ app/src/main/java/com/glints/lingoparents/ui/accountsetting/profile/ProfileFragment.kt
@@ -6,13 +6,72 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.glints.lingoparents.R
+import com.glints.lingoparents.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var binding : FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
+
+        binding.apply {
+            mbtnEdit.setOnClickListener {
+                enterEditState()
+            }
+            mbtnCancel.setOnClickListener {
+                exitEditState()
+            }
+            mbtnSave.setOnClickListener {
+                exitEditState()
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun enterEditState() {
+        binding.apply {
+            View.VISIBLE.apply {
+                tfAddress.visibility = this
+                tfFullName.visibility = this
+                tfPhoneNumber.visibility = this
+                mbtnSave.visibility = this
+                mbtnCancel.visibility = this
+            }
+            View.INVISIBLE.apply {
+                tvAddressContent.visibility = this
+                tvFullNameContent.visibility = this
+                tvPhoneNumberContent.visibility = this
+                mbtnLogout.visibility = this
+                mbtnEdit.visibility = this
+            }
+        }
+    }
+
+    private fun exitEditState() {
+        binding.apply {
+            View.VISIBLE.apply {
+                tvAddressContent.visibility = this
+                tvFullNameContent.visibility = this
+                tvPhoneNumberContent.visibility = this
+                mbtnLogout.visibility = this
+                mbtnEdit.visibility = this
+            }
+            View.INVISIBLE.apply {
+                tfAddress.visibility = this
+                tfAddress.editText?.setText("")
+                tfFullName.visibility = this
+                tfFullName.editText?.setText("")
+                tfPhoneNumber.visibility = this
+                tfPhoneNumber.editText?.setText("")
+                mbtnSave.visibility = this
+                mbtnCancel.visibility = this
+            }
+        }
     }
 }
