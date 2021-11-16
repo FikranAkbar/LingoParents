dc465c796c8c64b639198e91d18870ec3bf6b134
Fikran Akbar <fikranakbar756@gmail.com>
2021-11-16 23:33:52 +0700

    add login and register user api service

--- app/src/main/java/com/glints/lingoparents/data/api/APIService.kt
+++ app/src/main/java/com/glints/lingoparents/data/api/APIService.kt
@@ -1,5 +1,27 @@
 package com.glints.lingoparents.data.api
 
-interface APIService {
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
+interface APIService {
+    @FormUrlEncoded
+    @POST("api/v1/login")
+    fun loginUser(
+        @Field("email") email: String,
+        @Field("password") password: String
+    )
+
+    @FormUrlEncoded
+    @POST
+    fun registerUser(
+        @Field("email") email: String,
+        @Field("firstname") firstname: String,
+        @Field("lastname") lastname: String,
+        @Field("password") password: String,
+        @Field("phone") phone: String,
+        @Field("address") address: String,
+        @Field("gender") gender: String = "Male",
+        @Field("role") role: String = "parent"
+    )
 }
