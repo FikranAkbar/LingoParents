18ae566bd8c16d792a5e19ae421cf2008cba94d8
mahendramin <mahendramin19@gmail.com>
2021-12-06 15:28:28 +0700

    create error handling for course list

--- app/src/main/java/com/glints/lingoparents/ui/course/AllCoursesFragment.kt
+++ app/src/main/java/com/glints/lingoparents/ui/course/AllCoursesFragment.kt
@@ -23,83 +23,6 @@ import com.glints.lingoparents.utils.TokenPreferences
 import com.glints.lingoparents.utils.dataStore
 import kotlinx.coroutines.flow.collect
 
-//blm connect api
-//class AllCoursesFragment : Fragment(R.layout.fragment_all_courses) {
-//    private lateinit var rvCourse: RecyclerView
-//    private val list = ArrayList<CourseItem>()
-//    private var _binding: FragmentAllCoursesBinding? = null
-//    private val binding get() = _binding!!
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        _binding = FragmentAllCoursesBinding.bind(view)
-//        rvCourse = binding!!.rvCourse
-//        rvCourse.setHasFixedSize(true)
-////        list.clear()
-////        list.addAll(listCourse)
-//        showRecyclerList()
-//        Log.d("Coba", list.toString())
-//        //binding!!.iddarixml
-//        //binding!!.textView2.setText("hahahaha")
-//    }
-//
-//    private val listCourse: ArrayList<CourseItem>
-//        get() {
-//            val dataName = resources.getStringArray(R.array.data_course)
-//            val dataDesc1 = resources.getStringArray(R.array.data_desccourse1)
-//            val dataDesc2 = resources.getStringArray(R.array.data_desccourse2)
-//            val dataDesc3 = resources.getStringArray(R.array.data_desccourse3)
-//            val dataCard1 = resources.obtainTypedArray(R.array.data_coursecardphoto1)
-//            val dataCard2 = resources.obtainTypedArray(R.array.data_coursecardphoto2)
-//            val dataCard3 = resources.obtainTypedArray(R.array.data_coursecardphoto3)
-//            val dataPhoto = resources.obtainTypedArray(R.array.data_coursephoto)
-//            val listCourse = ArrayList<CourseItem>()
-//            for (i in dataName.indices) {
-//                val course = CourseItem(
-//                    dataName[i],
-//                    dataDesc1[i],
-//                    dataDesc2[i],
-//                    dataDesc3[i],
-//                    dataCard1.getResourceId(i, -1),
-//                    dataCard2.getResourceId(i, -1),
-//                    dataCard3.getResourceId(i, -1),
-//                    dataPhoto.getResourceId(i, -1)
-//                )
-//                listCourse.add(course)
-//            }
-//            return listCourse
-//        }
-//
-//    private fun showRecyclerList() {
-//        rvCourse.layoutManager = LinearLayoutManager(activity)
-//        val listCourseAdapter = CourseAdapter(list)
-//        rvCourse.adapter = listCourseAdapter
-//
-//        listCourseAdapter.setOnItemClickCallback(object : CourseAdapter.OnItemClickCallback {
-//            override fun onItemClicked(course: CourseItem) {
-//                //Toast.makeText(context, "Kamu memilih " + course.name, Toast.LENGTH_SHORT).show()
-//                goToDetail(course)
-//            }
-//        })
-//        list.clear()
-//        list.addAll(listCourse)
-//        listCourseAdapter.notifyDataSetChanged()
-//    }
-//
-//    private fun goToDetail(course: CourseItem) {
-//        val toDetailCourseFragment =
-//            AllCoursesFragmentDirections.actionAllCoursesFragmentToDetailCourseFragment(
-//                course
-//            )
-//        findNavController().navigate(toDetailCourseFragment)
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        _binding = null
-//    }
-//}
-
-//ini yang kalo udah connect pi
 class AllCoursesFragment : Fragment(R.layout.fragment_all_courses),
     CourseAdapter.OnItemClickCallback {
     private var _binding: FragmentAllCoursesBinding? = null
@@ -145,7 +68,7 @@ class AllCoursesFragment : Fragment(R.layout.fragment_all_courses),
                 when (event) {
                     is AllCoursesViewModel.AllCoursesEvent.Loading -> {
                         showLoading(true)
-//                        showEmptyWarning(false)
+                        showEmptyWarning(false)
                     }
                     is AllCoursesViewModel.AllCoursesEvent.Success -> {
                         courseAdapter.submitList(event.list)
@@ -153,7 +76,7 @@ class AllCoursesFragment : Fragment(R.layout.fragment_all_courses),
                     }
                     is AllCoursesViewModel.AllCoursesEvent.Error -> {
                         showLoading(false)
-//                        showEmptyWarning(true)
+                        showEmptyWarning(true)
                     }
 //                    is AllCoursesViewModel.AllCoursesEvent.NavigateToDetailLiveEventFragment -> {
 //                        val action =
@@ -183,23 +106,23 @@ class AllCoursesFragment : Fragment(R.layout.fragment_all_courses),
         binding.apply {
             if (bool) {
                 rvCourse.visibility = View.GONE
-//                shimmerLayout.visibility = View.VISIBLE
+                shimmerLayout.visibility = View.VISIBLE
             } else {
                 rvCourse.visibility = View.VISIBLE
-//                shimmerLayout.visibility = View.GONE
+                shimmerLayout.visibility = View.GONE
             }
         }
     }
 
-//    private fun showEmptyWarning(bool: Boolean) {
-//        binding.apply {
-//            if (bool) {
-//                ivNoEvent.visibility = View.VISIBLE
-//                tvNoEvent.visibility = View.VISIBLE
-//            } else {
-//                ivNoEvent.visibility = View.GONE
-//                tvNoEvent.visibility = View.GONE
-//            }
-//        }
-//    }
+    private fun showEmptyWarning(bool: Boolean) {
+        binding.apply {
+            if (bool) {
+                ivNoCourse.visibility = View.VISIBLE
+                tvNoCourse.visibility = View.VISIBLE
+            } else {
+                ivNoCourse.visibility = View.GONE
+                tvNoCourse.visibility = View.GONE
+            }
+        }
+    }
 }
