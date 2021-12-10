94e261db49d12d935c393da0a953d32028ca8f77
Fikran Akbar <fikranakbar756@gmail.com>
2021-12-10 16:46:44 +0700

    implementing course list api to make tab item

--- app/src/main/java/com/glints/lingoparents/ui/progress/learning/ProgressLearningFragment.kt
+++ app/src/main/java/com/glints/lingoparents/ui/progress/learning/ProgressLearningFragment.kt
@@ -1,13 +1,24 @@
 package com.glints.lingoparents.ui.progress.learning
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
+import coil.load
 import com.glints.lingoparents.R
+import com.glints.lingoparents.data.model.response.CourseListByStudentIdResponse
 import com.glints.lingoparents.databinding.FragmentProgressLearningBinding
 import com.glints.lingoparents.ui.progress.ProgressViewModel
 import com.glints.lingoparents.utils.CustomViewModelFactory
@@ -15,6 +26,7 @@ import com.glints.lingoparents.utils.TokenPreferences
 import com.glints.lingoparents.utils.dataStore
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.flow.collect
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 
@@ -47,21 +59,76 @@ class ProgressLearningFragment : Fragment(R.layout.fragment_progress_learning) {
                 ProgressLearningViewModel::class.java
         ]
 
-        initViewPager()
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.progressLearningEvent.collect { event ->
+                when(event) {
+                    is ProgressLearningViewModel.ProgressLearningEvent.Loading -> {
+
+                    }
+                    is ProgressLearningViewModel.ProgressLearningEvent.Success -> {
+                        initViewPager(event.result)
+                    }
+                    is ProgressLearningViewModel.ProgressLearningEvent.Error -> {
+
+                    }
+                }
+            }
+        }
     }
 
-    private fun initViewPager() {
-        val tabIcons = arrayOf(R.drawable.ic_small_korea_flag, R.drawable.ic_small_germany_flag)
+    private fun initViewPager(courseList: List<CourseListByStudentIdResponse.DataItem>) {
         val sectionsPagerAdapter = ProgressCourseSectionPagerAdapter(activity as AppCompatActivity)
         val viewPager: ViewPager2 = binding.viewPager
         viewPager.isUserInputEnabled = false
         viewPager.adapter = sectionsPagerAdapter
+
         val tabs: TabLayout = binding.tlCourseList
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
-            tab.text = resources.getString(TAB_TITLES[position])
-        }.attach()
-        tabs.getTabAt(0)!!.setIcon(tabIcons[0])
-        tabs.getTabAt(1)!!.setIcon(tabIcons[1])
+
+        if (courseList.isNotEmpty()) {
+            TabLayoutMediator(tabs, viewPager) { tab, position ->
+                tab.customView = createTabItemView(courseList[position], position)
+            }.attach()
+        }
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.view?.apply {
+                    findViewById<TextView>(R.id.tv_flag)
+                        ?.setTextColor(Color.BLACK)
+                    findViewById<ImageView>(R.id.iv_flag)
+                        ?.clearColorFilter()
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.view?.apply {
+                    findViewById<TextView>(R.id.tv_flag)
+                        ?.setTextColor(Color.parseColor("#B8B8BA"))
+                    findViewById<ImageView>(R.id.iv_flag)
+                        ?.setColorFilter(Color.parseColor("#B8B8BA"), PorterDuff.Mode.ADD)
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+    @SuppressLint("InflateParams")
+    private fun createTabItemView(courseData: CourseListByStudentIdResponse.DataItem, position: Int): View {
+        val tabItem = LayoutInflater.from(requireContext()).inflate(R.layout.item_tab_course, null, false) as ConstraintLayout
+        val tabLabel = tabItem.findViewById<TextView>(R.id.tv_flag)
+        val tabFlag = tabItem.findViewById<ImageView>(R.id.iv_flag)
+
+        tabLabel.text = courseData.title
+        tabFlag.load(courseData.flag)
+
+        if (position != 0) {
+            tabLabel.setTextColor(Color.parseColor("#B8B8BA"))
+            tabFlag.setColorFilter(Color.parseColor("#B8B8BA"), PorterDuff.Mode.ADD)
+        }
+
+        return tabItem
     }
 
     @Subscribe(sticky = true)
