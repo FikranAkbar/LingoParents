7a44df53150e845e9f1eb8bf63345e9d3dd28c7c
Fikran Akbar <fikranakbar756@gmail.com>
2021-12-10 10:34:06 +0700

    fix progress fragment

--- app/src/main/java/com/glints/lingoparents/ui/progress/ProgressFragment.kt
+++ app/src/main/java/com/glints/lingoparents/ui/progress/ProgressFragment.kt
@@ -1,7 +1,6 @@
 package com.glints.lingoparents.ui.progress
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
