882496ba152fd88b7f3e7b2ee5e30d5f60fc4c3d
mahendramin <mahendramin19@gmail.com>
2021-12-15 00:33:05 +0700

    create api service for register live event and its response

---
+++ app/src/main/java/com/glints/lingoparents/data/model/response/LiveEventRegisterResponse.kt
@@ -0,0 +1,42 @@
+package com.glints.lingoparents.data.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LiveEventRegisterResponse(
+
+	@field:SerializedName("total_price")
+	val totalPrice: Int,
+
+	@field:SerializedName("phone")
+	val phone: String,
+
+	@field:SerializedName("voucher_code")
+	val voucherCode: String,
+
+	@field:SerializedName("id_user")
+	val idUser: Int,
+
+	@field:SerializedName("id_event")
+	val idEvent: Int,
+
+	@field:SerializedName("fullname")
+	val fullname: String,
+
+	@field:SerializedName("attendance_time")
+	val attendanceTime: String,
+
+	@field:SerializedName("email")
+	val email: String,
+
+	@field:SerializedName("attendance")
+	val attendance: String,
+
+	@field:SerializedName("payment_method")
+	val paymentMethod: String,
+
+	@field:SerializedName("idUser_create")
+	val idUserCreate: Int,
+
+	@field:SerializedName("status")
+	val status: String
+)
