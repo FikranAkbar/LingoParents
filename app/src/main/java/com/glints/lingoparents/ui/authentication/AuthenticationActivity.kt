226d632150996366b236b3319bab2fa25ed00288
Fikran Akbar <fikranakbar756@gmail.com>
2022-01-17 14:29:43 +0700

    remove unnecessary register user flag

--- app/src/main/java/com/glints/lingoparents/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/glints/lingoparents/ui/authentication/AuthenticationActivity.kt
@@ -1,6 +1,5 @@
 package com.glints.lingoparents.ui.authentication
 
-import android.app.Activity
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -23,12 +22,16 @@ class AuthenticationActivity : AppCompatActivity() {
         val flag = intent.getIntExtra("flag", -1)
         if (flag == TOKEN_EXPIRED_FLAG) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                Snackbar.make(binding.root, getString(R.string.token_expired_error), Snackbar.LENGTH_SHORT)
+                Snackbar.make(binding.root,
+                    getString(R.string.token_expired_error),
+                    Snackbar.LENGTH_SHORT)
                     .setBackgroundTint(resources.getColor(R.color.error_color, null))
                     .setTextColor(Color.WHITE)
                     .show()
             } else {
-                Snackbar.make(binding.root, getString(R.string.token_expired_error), Snackbar.LENGTH_SHORT)
+                Snackbar.make(binding.root,
+                    getString(R.string.token_expired_error),
+                    Snackbar.LENGTH_SHORT)
                     .setBackgroundTint(Color.RED)
                     .setTextColor(Color.WHITE)
                     .show()
@@ -36,5 +39,3 @@ class AuthenticationActivity : AppCompatActivity() {
         }
     }
 }
-
-const val REGISTER_USER_RESULT_OK = Activity.RESULT_FIRST_USER + 1
