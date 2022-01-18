bae677def591b2023950f05b34274f12faf1cba0
Fikran Akbar <fikranakbar756@gmail.com>
2022-01-18 23:47:55 +0700

    add insight comment item class to simplify comment response format

---
+++ app/src/main/java/com/glints/lingoparents/data/model/InsightCommentItem.kt
@@ -0,0 +1,12 @@
+package com.glints.lingoparents.data.model
+
+data class InsightCommentItem(
+    val idComment: Int,
+    val idUser: Int,
+    val photo: String?,
+    val name: String,
+    val comment: String,
+    val totalLike: Int,
+    val totalDislike: Int,
+    val totalReplies: Int
+)
