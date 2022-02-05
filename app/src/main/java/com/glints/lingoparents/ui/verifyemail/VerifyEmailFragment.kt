9a52b355e6e0944757c70db576653fc1977ab9d5
Fikran Akbar <fikranakbar756@gmail.com>
2022-02-05 20:22:45 +0700

    add new verify email screen

---
+++ app/src/main/java/com/glints/lingoparents/ui/verifyemail/VerifyEmailFragment.kt
@@ -0,0 +1,8 @@
+package com.glints.lingoparents.ui.verifyemail
+
+import androidx.fragment.app.Fragment
+import com.glints.lingoparents.R
+
+class VerifyEmailFragment : Fragment(R.layout.fragment_verify_email) {
+
+}
